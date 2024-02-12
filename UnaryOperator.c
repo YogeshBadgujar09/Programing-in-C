fbab08871998093a19ad262af9d26a775ec8885b
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-12 19:12:02 +0530

    [ADDED] : YOGESH unary Operater program

---
+++ UnaryOperator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+	int a=1 ;
+ 
+	
+	printf("\n%d",++a);		//	a=a+1 ;		a=1+1 ;	  2
+	printf("\n%d",a++);		//	a=a+1 ;		1=1+1 ;   2
+	printf("\n%d",--a);
+	printf("\n%d",a--);
+	
+ 
+}
