390a1efdf05437fd939edc20c5f7fc4b3b8902ab
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-14 10:12:20 +0530

    [ADDED] : YOGESH Role of Curly braces in Program

---
+++ ScopeRoleInProgram.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int third =30;
+	int first =20;
+	int second =10;
+	
+	{
+		int third = first - second ;
+		
+		printf("%d",third)	;
+	}
+	
+		printf("\n%d",third)	;
+}
