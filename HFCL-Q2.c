2cc557e29dfc121e390e222154f026eeea8efa9e
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-09-22 23:02:36 +0530

    [ADDED] : Yogesh - HFCL - Round Question

---
+++ HFCL-Q2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int i =10 ;
+void main() 
+{
+    int i =20,n; 
+   
+    for(n=0;n<=i;) 
+    {
+        int i=10; 
+        i++; 
+    } 
+   // printf("%d",i);
+   printf("%d",i);
+}
